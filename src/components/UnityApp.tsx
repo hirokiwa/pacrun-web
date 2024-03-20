9780dc30250762b76805b02dab33c83021aab76a
hirokiwa <hirokifsys@gmail.com>
2024-03-20 14:04:54 +0900

    Unity Build 5db1a97e531d486156c4b29f47169b1a2fc7897a

--- src/components/UnityApp.tsx
+++ src/components/UnityApp.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 export default function UnityApp() {
   const { unityProvider } = useUnityContext({
-  loaderUrl: "pacrun-webgl-package4/Build/pacrun-webgl-package4.loader.js",
-  dataUrl: "pacrun-webgl-package4/Build/pacrun-webgl-package4.data",
-  frameworkUrl: "pacrun-webgl-package4/Build/pacrun-webgl-package4.framework.js",
-  codeUrl: "pacrun-webgl-package4/Build/pacrun-webgl-package4.wasm",
-  streamingAssetsUrl: "pacrun-webgl-package4/StreamingAssets",
+  loaderUrl: "pacrun-webgl-package5/Build/pacrun-webgl-package5.loader.js",
+  dataUrl: "pacrun-webgl-package5/Build/pacrun-webgl-package5.data",
+  frameworkUrl: "pacrun-webgl-package5/Build/pacrun-webgl-package5.framework.js",
+  codeUrl: "pacrun-webgl-package5/Build/pacrun-webgl-package5.wasm",
+  streamingAssetsUrl: "pacrun-webgl-package5/StreamingAssets",
 });
   return (
     <Unity unityProvider={unityProvider} style={{ width: "100%", height: "100vh" }} />
